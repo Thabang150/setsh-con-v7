6a4dd217e2154cef31a08118d86af2048f16905f
Thabang150 <thabang@tmtr20.com>
2025-09-27 23:13:49 +0200

    Updated supabase.js

--- citizens-app/src/config/supabase.js
+++ citizens-app/src/config/supabase.js
@@ -1,10 +1,11 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env'; // <-- correct import
+// import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env'; // <-- correct import
 
 SUPABASE_URL="https://qlxwzpnslabmckzogqit.supabase.co"
   SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFseHd6cG5zbGFibWNrem9ncWl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MDI5MzAsImV4cCI6MjA3NDI3ODkzMH0.IXiess17sPAh9XA6rcV6yijUNxyySLIS4hisJTMbTCg"
+
 console.log('Supabase URL:', SUPABASE_URL);
 console.log('Supabase ANON Key:', SUPABASE_ANON_KEY);
 
